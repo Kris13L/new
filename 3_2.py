108879a34398928198081b3f2f237e94c0bac59c
Kris13L <kri13md@icloud.com>
2022-09-22 18:47:36 +0300

    task №2 solution

---
+++ 3_2.py
@@ -0,0 +1,6 @@
+first_value = int(input('Введите первое число: '))
+second_value = int(input('Введите второе число: '))
+third_value = int(input('Введите третье число: '))
+values = [first_value, second_value, third_value]
+average = sum(values) / len(values)
+print(average)
