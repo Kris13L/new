8ec4388151b5d7a7f78f674117e16550758970e9
Kris13L <kri13md@icloud.com>
2022-09-25 14:28:51 +0300

    task №3

---
+++ 3_3.py
@@ -0,0 +1,8 @@
+name = input('Name:')
+age = input('Age:')
+town = input('Town:')
+
+print(f"Меня зовут {name}. Мне {age} лет. Я из города {town}.")
+
+print('Меня зовут {}. Мне {} лет. Я из города {}.'.format(name, age, town))
+
